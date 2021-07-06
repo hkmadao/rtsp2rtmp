cd26598f0abd9d16b82b888d78f0db1264f39aee
hkmadao <madaodesuka@hotmail.com>
2021-07-06 11:25:44 +0800

    build脚本优化

--- build.bat
+++ build.bat
@@ -2,22 +2,22 @@
 chcp 65001
 set /p ver=请输入版本：  
 echo 版本：%ver% 打包开始
-rmdir /S /Q .\resources\output\releases
+rmdir /S /Q .\resources\output\demo
 @REM windows_amd64
 echo 打包windows_amd64平台
 SET GOOS=windows
 SET GOARCH=amd64
-SET CGO_ENABLED=0
-go build -o .\resources\output\releases\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\rtmp2flv.exe main.go
+SET CGO_ENABLED=1
+go build -o .\resources\output\demo\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\rtmp2flv.exe main.go
 echo =============%GOOS%_%GOARCH%
-md .\resources\output\releases\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\output\live
-md .\resources\output\releases\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\output\log
-md .\resources\output\releases\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\conf
+md .\resources\output\demo\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\resources\output\live
+md .\resources\output\demo\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\resources\output\log
+md .\resources\output\demo\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\resources\conf
 
-xcopy /S /Y /E .\resources\static .\resources\output\releases\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\static\
-xcopy /S /Y /E .\resources\db .\resources\output\releases\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\db\
-xcopy /S /Y /E .\resources\conf .\resources\output\releases\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\conf
-cd .\resources\output\releases\
+xcopy /S /Y /E .\resources\static .\resources\output\demo\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\resources\static\
+xcopy /S /Y /E .\resources\db .\resources\output\demo\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\resources\db\
+xcopy /S /Y /E .\resources\conf .\resources\output\demo\rtsp2rtmp_%ver%_%GOOS%_%GOARCH%\resources\conf
+cd .\resources\output\demo\
 7z a -ttar -so rtsp2rtmp_%ver%_%GOOS%_%GOARCH%_demo.tar rtsp2rtmp_%ver%_%GOOS%_%GOARCH%/ | 7z a -si rtsp2rtmp_%ver%_%GOOS%_%GOARCH%_demo.tar.gz
 cd ..\..\..\
 
--- build.sh
+++ build.sh
@@ -9,28 +9,27 @@ else
 fi
 #打多个平台的包
 platforms="linux_amd64"
-rm -rf ./resources/output/releases/
+rm -rf ./resources/output/demo/
 for platform in $platforms; do
 
     export GOOS=$(echo "$platform" | gawk 'BEGIN{FS="_"} {print $1}')
     export GOARCH=$(echo "$platform" | gawk 'BEGIN{FS="_"} {print $2}')
-    export CGO_ENABLED=0
+    export CGO_ENABLED=1
     echo "${GOOS}"_"${GOARCH}"
     if [[ "${GOOS}" == "windows" ]]; then
-        go build -o ./resources/output/releases/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/rtsp2rtmp.exe main.go
+        go build -o ./resources/output/demo/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/rtsp2rtmp.exe main.go
     else
-        go build -o ./resources/output/releases/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/rtsp2rtmp main.go
+        go build -o ./resources/output/demo/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/rtsp2rtmp main.go
     fi
-    go build -o ./resources/output/releases/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/rtsp2rtmp main.go
 
-    mkdir -p ./resources/output/releases/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/output/live
-    mkdir -p ./resources/output/releases/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/output/log
-    mkdir -p ./resources/output/releases/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/conf
+    mkdir -p ./resources/output/demo/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/output/live
+    mkdir -p ./resources/output/demo/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/output/log
+    mkdir -p ./resources/output/demo/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/conf
 
-    cp -r ./resources/static ./resources/output/releases/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/static/
-    cp -r ./resources/conf ./resources/output/releases/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/conf
+    cp -r ./resources/static ./resources/output/demo/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/static/
+    cp -r ./resources/conf ./resources/output/demo/rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/resources/conf
 
-    cd ./resources/output/releases/ || exit
+    cd ./resources/output/demo/ || exit
     rm -rf rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}".tar.gz
     tar -zcvf ./rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}".tar.gz rtsp2rtmp_"${ver}"_"${GOOS}"_"${GOARCH}"/
 
